5db2732bcd0388c0834a9ebd3f2ec9f4afb10cf3
CypherPotato <gscatolin@outlook.com>
2024-04-11 01:10:20 -0300

    initial and maybe the unique commit

---
+++ CommandLineParser.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandLine
+{
+    /// <summary>
+    /// Provides an command-line interpreter.
+    /// </summary>
+    public class CommandLineParser
+    {
+        /// <summary>
+        /// Gets the input arguments used to construct this interpreter.
+        /// </summary>
+        public string[] Arguments { get; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="System.StringComparison"/> used for verb comparisons.
+        /// </summary>
+        public StringComparison Comparer { get; set; } = StringComparison.InvariantCultureIgnoreCase;
+
+        /// <summary>
+        /// Creates an new instance of the <see cref="CommandLineParser"/> with given command-line parameters.
+        /// </summary>
+        /// <param name="args">An array of command-line parameters.</param>
+        public CommandLineParser(string[] args)
+        {
+            Arguments = args;
+        }
+
+        /// <summary>
+        /// Creates an new instance of the <see cref="CommandLineParser"/> with given command-line parameters.
+        /// </summary>
+        /// <param name="args">An array of command-line parameters.</param>
+        /// <param name="comparer">Represents the <see cref="System.StringComparison"/> used for verb comparisons.</param>
+        public CommandLineParser(string[] args, StringComparison comparer)
+        {
+            Arguments = args;
+            Comparer = comparer;
+        }
+
+        /// <summary>
+        /// Returns all values that are part of the specified switch. The search searches all preceding values of the
+        /// specified verb, and stops in the presence of an antecedent verb.
+        /// </summary>
+        /// <param name="longVerb">The verb in it's long form.</param>
+        /// <param name="shortVerb">The verb in it's short form.</param>
+        public IEnumerable<string> GetValues(string longVerb, char? shortVerb = null)
+        {
+            bool insertingNext = false;
+            for (int i = 0; i < Arguments.Length; i++)
+            {
+                string item = Arguments[i];
+                if (insertingNext)
+                {
+                    if (item.StartsWith("-"))
+                    {
+                        insertingNext = false;
+                    }
+                    else
+                    {
+                        yield return item;
+                    }
+                }
+
+                if (IsVerbMatch(item, longVerb, shortVerb))
+                {
+                    insertingNext = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the first value found that is preceded by the specified verb.
+        /// </summary>
+        /// <param name="longVerb">The verb in it's long form.</param>
+        /// <param name="shortVerb">The verb in it's short form.</param>
+        /// <returns>The value preceded by the specified verb, or null if the verb is not defined or has no value.</returns>
+        public string? GetValue(string longVerb, char? shortVerb = null)
+        {
+            for (int i = 0; i < Arguments.Length; i++)
+            {
+                string item = Arguments[i];
+
+                if (IsVerbMatch(item, longVerb, shortVerb))
+                {
+                    return Lookup(i + 1);
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Returns an <see cref="bool"/> indicating whether the specified verb is present in the argument collection.
+        /// </summary>
+        /// <param name="longVerb">The verb in it's long form.</param>
+        /// <param name="shortVerb">The verb in it's short form.</param>
+        /// <returns>An <see cref="bool"/> indicating whether the specified verb is present in the argument collection.</returns>
+        public bool IsDefined(string longVerb, char? shortVerb = null)
+        {
+            for (int i = 0; i < Arguments.Length; i++)
+            {
+                string item = Arguments[i];
+
+                if (IsVerbMatch(item, longVerb, shortVerb))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns all values that are not verbs and that are not preceded by a verb.
+        /// </summary>
+        public IEnumerable<string> GetRemainder()
+        {
+            for (int i = 0; i < Arguments.Length; i++)
+            {
+                string? before = i == 0 ? null : Arguments[i - 1];
+                string item = Arguments[i];
+
+                bool beforeIsSwitch = before != null && before.StartsWith('-');
+
+                if (!beforeIsSwitch && !item.StartsWith('-'))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        bool IsVerbMatch(string verb, string longForm, char? shortForm)
+        {
+            if (!verb.StartsWith('-')) return false;
+            if (verb.StartsWith("--" + longForm, Comparer)) return true;
+            if (shortForm is char c && verb.StartsWith("-" + shortForm, Comparer)) return true;
+            return false;
+        }
+
+        string? Lookup(int index)
+        {
+            if (index >= 0 && index < Arguments.Length)
+                return Arguments[index];
+            return null;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,96 @@
+# CommandLine
+
+This tiny library has an purpose to extract values
+from the command line. It is not capable of interpreting
+subcommands and does not use reflection to extract data.
+It is so small that it contains a class and that
+class contains 2 methods and 2 properties.
+
+Consider this command line:
+
+```
+curl.exe -h "Content-Length: 0" -h "User-Agent: foo/1.0" -X POST https://example.com/posts
+```
+
+You can parse it and get each parameter with:
+
+```csharp
+static void Main(string[] args)
+{
+    var parser = new CommandLine.CommandLineParser(args, StringComparison.InvariantCulture);
+    
+    string[] headers = parser.GetValues("header", 'h').ToArray();
+    string? method = parser.GetValue("method", 'X');
+    string? url = parser.GetRemainder()?.LastOrDefault();
+        
+    Console.WriteLine("Headers  = {0}", string.Join(", ", headers));
+    Console.WriteLine("Method   = {0}", method);
+    Console.WriteLine("Url      = {0}", url);
+}
+```
+
+### Getting multiple values
+
+```
+example.exe -p param1 param2 param3 -d "another command" --parameter param4 --foobar
+```
+
+```csharp
+static void Main(string[] args)
+{
+    var parser = new CommandLine.CommandLineParser(args);
+    
+    string[] headers = parser.GetValues("parameter", 'p').ToArray();
+    // ["param1", "param2", "param3", "param4"]
+}
+```
+
+### Getting a single value
+
+```
+example.exe -d "command" --foo "hello"
+```
+
+```csharp
+static void Main(string[] args)
+{
+    var parser = new CommandLine.CommandLineParser(args);
+    
+    string? d = parser.GetValue("directive", 'd'); // "command"
+    string? f = parser.GetValue("foo", 'f'); // "hello"
+    string? x = parser.GetValue("empty"); // null
+}
+```
+
+### Check if has switch
+
+```
+example.exe --verbose
+```
+
+```csharp
+static void Main(string[] args)
+{
+    var parser = new CommandLine.CommandLineParser(args);
+    
+    bool verbose = parser.IsDefined("verbose"); // short-terms are optional for all methods
+}
+```
+
+### Get parameterless values
+
+```
+example.exe build --language "csharp" -r "System.dll" program.cs lib.cs --out item.cs foobar
+```
+
+```csharp
+static void Main(string[] args)
+{
+    var parser = new CommandLine.CommandLineParser(args);
+    
+    string[] remainder = parser.GetRemainder().ToArray();
+    // ["build", "program.cs", "lib.cs", "foobar"]
+}
+```
+
+And that's all this lib can do.
